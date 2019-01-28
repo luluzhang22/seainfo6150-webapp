53fd255fafc915e2d2f8c75ec71e0af06eceff5b
luluzhang22 <luluzhang0202@gmail.com>
2019-01-27 23:24:24 -0800

    assignment 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import List from './List';
+import Article from './Article';
 
 class App extends Component {
   render() {
@@ -10,10 +11,42 @@ class App extends Component {
       { firstName: "Mickey", lastName: "Mouse" }
     ];
 
+    const title = "The Statue of Liberty's torch heads to new museum";
+    const date = "November 22, 2018";
+    const author = "Kate Farley";
+
     return (
       <div className="App">
-        <List list={array} />
+        {/*<List list={array} />*/}
+        <div className="Assignment 3">
+            <Article title={title} date={date} author={author}>
+                <div>
+                    <p>
+                        The Statue of Liberty's original torch is getting a new home.
+                        Made of copper and gold so that it would shine bright, the first torch took a beating from the weather and was replaced by a replica in 1984. It's been stored in the statue's pedestal ever since.
+                        But visitors will be able to see the original 3,600-pound torch in all its glory at a new Statue of Liberty Museum, which will open on Liberty Island in May 2019. The torch was transported there by truck last week, along with a replica model of Lady Liberty's face.
+                    </p>
+                    <p>
+                        <q>Although it is not one of the most difficult things we have ever moved,</q> says Douglas Phelps, who oversaw the relocation of the torch, <q>it is certainly the most important.</q>
+                        Not only is the Statue of Liberty the symbol of New York City, she is also a UNESCO World Heritage site under the designation of <q>Outstanding Universal Value.</q>
+                        Her full name is Liberty Enlightening the World, and she was designed by Frederic Auguste Bartholdi and built by Gustave Eiffel -- yes, the same guy who built Paris's Eiffel Tower.
+                    </p>
+                    <p>
+                        Lady Liberty was a gift from France to the United States to celebrate the centennial of their nation's independence -- although, somewhat awkwardly, she arrived a decade too late. Nevertheless, President Grover Cleveland formally unveiled her in 1886.
+                        Since then, the statue has served as a symbol of hope and possibility for many people who entered the United States as immigrants via neighboring Ellis Island.
+                        <q>The original torch is a beautiful object of art and exemplifies the American ideals of freedom and democracy,</q> says John Piltzecker, superintendent of the Statue of Liberty National Monument. That symbolic spirit was bolstered by the decision to inscribe Emma Lazarus' poem <q>The New Colossus</q> on the statue's pedestal, which gave Lady Liberty one of her nicknames, <q>Mother of Exiles.</q>
+                        The most famous lines in the poem are: <q>Give me your tired, your poor / Your huddled masses yearning to breathe free.</q>
+                    </p>
+                    <p>
+                        In 2017, nearly 4.5 million people visited Liberty Island.
+                        When completed, the Statue of Liberty Museum will be 26,000 square feet of history, inspiration and deeper analysis of the statue's symbolism both in the US and around the world.
+                        The <q>inspiration gallery,</q> which is where the torch will live, will have floor-to-ceiling glass windows looking out at Lower Manhattan, including the World Trade Center.
+                    </p>
+                </div>
+            </Article>
+        </div>
       </div>
+
     );
   }
 }
